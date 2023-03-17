d82ccfc6f5d760d0a115cfdd97154f5e3fecc71a
guilt98 <124409817+guilt98@users.noreply.github.com>
2023-03-16 21:22:50 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="resumo12">
+      <header className="resumo12">
+      <h1>Um ataque em Shirbit</h1>
+      <p>No segundo trimestre de 2021 um nove ataque do grupo de hackers da <b>BlackShadow</b>.Eles ficaram conhecidos por terem vazados inumeros documentos secretos e seguros <b>Shirbit</b> eles desenvolveram um malware que restringe o acesso a um tipo de sistema ate que resgate de criptomoedas seja pago pela vitima,desta vez os dados foram divulgados no telegram</p>
+      <p>Os pesquisadores da <b>Kaspersky</b> viram que os atores usaram um servidor C2 que foi contactado por um implante <b>Android</b> malicioso. Essa ferramente foi usada em outros paises do oriente medio na campanha  chamada <b>WildPressure</b>.</p>
+      <p>A campanha de ataques ciberneticos usou a linguagem python para desenvolver um malware que afetasse os sistemas <b>Windows e MacOs</b>. Porem mesmo com as evidencias nao se pode acusar a BlackShadow ou relacionar ela a essa tecnologia.</p>
+      <h2 className='h2'>Caracteristicas.</h2>
+      <div>
+      <ul>
+        <li>Intenção:O ataque visou chantagear a seguradora ameaçando comercializar os dados dos clientes caso eles nao pagassem a quantia exigida. </li>
+        <li>Metódo:Utilizando um malware que afetasse os sistemas operacinais para poder ter acesso a todas as informaçoes sigilosas dos clientes da seguradora.</li>
+        <li>Como:Não a detalhes de quanto tempo ou como e quanto teriam feito o ataque,a seguradora apenas disse ter recebido uma carta avisando do ataque e o valor a ser pago.</li>
+      </ul>
+      </div>
+     <h2>Pilares afetados</h2>
+    <div>
+     <ol>
+      <li>Confidencialidade:roubando dados sigilosos dos clientes para usar como moeda de troca</li>
+      <li>integridade:Apesar de nao ter sido feito,por terem tido acesso aos dados dos clientes poderiam fazer auteraçoes em seus cadastros e dados ali mantidos.</li>
+      <li>Autenticidade:Como acabou se tornando nao seguro a fonte de acesso da empresa e acabando acarretando na paralisação do serviço ate que fosse concertado os danos feito,para evitar o pior.</li>
+     </ol>
+     </div>
+
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
